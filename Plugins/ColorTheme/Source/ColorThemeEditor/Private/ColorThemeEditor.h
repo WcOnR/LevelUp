85d1e6452c7673e32763702878ab0bdf3648ddc6
Danil Loyk <19wconr93@gmail.com>
2023-03-09 21:31:31 +0100

    fixed multiple include

--- Plugins/ColorTheme/Source/ColorThemeEditor/Private/ColorThemeEditor.h
+++ Plugins/ColorTheme/Source/ColorThemeEditor/Private/ColorThemeEditor.h
@@ -1,4 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
+#pragma once
+
 #include "Containers/Array.h"
 #include "Modules/ModuleInterface.h"
 #include "Modules/ModuleManager.h"
--- Plugins/ColorTheme/Source/ColorThemeEditor/Private/Tools/ColorThemeTool.h
+++ Plugins/ColorTheme/Source/ColorThemeEditor/Private/Tools/ColorThemeTool.h
@@ -1,4 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
+#pragma once
+
 #include "Containers/Array.h"
 #include "IToolInterface.h"
 
